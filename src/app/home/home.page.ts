ac4ff91d8af6922b69d7be4bbe9271913fabd0b7
markzuck3rberg <141587136+markzuck3rberg@users.noreply.github.com>
2024-02-07 18:57:16 -0500

    último cambio

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,6 +11,7 @@ export class HomePage {
   private tableled1: any;
   private tableled2: any;
   private tableled3: any;
+  private tableled4: any;
 
   constructor(private db: Firestore) {
     this.saveStateData();
@@ -19,6 +20,7 @@ export class HomePage {
   estadoLed1: boolean = false;
   estadoLed2: boolean = false;
   estadoLed3: boolean = false;
+  estadoLed4: boolean = false;
 
   async encender(led: string) {
     let tableled: any;
@@ -33,6 +35,9 @@ export class HomePage {
     } else if (led === 'LED3') {
       tableled = this.tableled3;
       estadoLed = this.estadoLed3;
+    }else if (led === 'LED4') {
+      tableled = this.tableled4;
+      estadoLed = this.estadoLed4;
     }
 
     tableled = doc(this.db, 'controlLED/' + led);
@@ -54,6 +59,9 @@ export class HomePage {
     } else if (led === 'LED3') {
       tableled = this.tableled3;
       estadoLed = this.estadoLed3;
+    }else if (led === 'LED4') {
+      tableled = this.tableled4;
+      estadoLed = this.estadoLed4;
     }
 
     tableled = doc(this.db, 'controlLED/' + led);
@@ -66,10 +74,12 @@ export class HomePage {
     const docref1 = doc(this.db, 'controlLED', 'LED1');
     const docref2 = doc(this.db, 'controlLED', 'LED2');
     const docref3 = doc(this.db, 'controlLED', 'LED3');
+    const docref4 = doc(this.db, 'controlLED', 'LED4');
 
     const snap1 = await getDoc(docref1);
     const snap2 = await getDoc(docref2);
     const snap3 = await getDoc(docref3);
+    const snap4 = await getDoc(docref4);
 
     if (snap1.exists()) {
       this.estadoLed1 = snap1.data()['encender'];
@@ -82,6 +92,10 @@ export class HomePage {
     if (snap3.exists()) {
       this.estadoLed3 = snap3.data()['encender'];
     }
+
+    if (snap4.exists()) {
+      this.estadoLed4 = snap4.data()['encender'];
+    }
   }
 
   actualizarEstado(led: string, estado: boolean) {
@@ -91,12 +105,14 @@ export class HomePage {
       this.estadoLed2 = estado;
     } else if (led === 'LED3') {
       this.estadoLed3 = estado;
+    }else if (led === 'LED4') {
+      this.estadoLed4 = estado;
     }
   }
 
   getColor(estado: boolean): string {
-    return estado ? 'success' : 'danger';
-  }
+    return estado ? 'success' : 'danger';
+  }
 }
 
 
